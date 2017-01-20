d2c8a8b897ab08f839bf844bdee962ca0e9c813e
Kyp01Gaming <agm.l@hotmail.com>
2017-01-21 00:20:21 +1100

    Packing Update/Test

---
+++ Project/Assets/Custom/Scripts/RectangleTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class RectangleTest : MonoBehaviour {
+
+    MaxRectsBinPack bin;
+    
+    public List<RectTransform> rects;
+
+    public RectTransform binRect;
+    public MaxRectsBinPack.FreeRectChoiceHeuristic heu;
+
+    void Awake()
+    {
+        bin = new MaxRectsBinPack(Mathf.RoundToInt(binRect.lossyScale.x), Mathf.RoundToInt(binRect.lossyScale.y));
+    }
+
+    void Start()
+    {
+        foreach (RectTransform rect in rects)
+        {
+            Rect r = bin.Insert(Mathf.RoundToInt(rect.localScale.x), Mathf.RoundToInt(rect.localScale.y), heu);
+            rect.localPosition = new Vector3(r.x, r.y, 0);
+            rect.localScale = new Vector3(r.width, r.height, 1);
+        }
+    }
+}
